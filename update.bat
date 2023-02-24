62a9519bfdca6d5311d9637b50020cdb9a4b5570
derrian-distro <121591654+derrian-distro@users.noreply.github.com>
2023-02-24 02:28:52 -0500

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,13 +1,6 @@
 @echo off
 
-set "scriptDir=%~dp0"
-set "scriptDir=%scriptDir:~0,-1%"
-
-git config --global --add safe.directory %scriptDir%
 git pull
 git submodule init
 git submodule update
-goto end
-
-:end
 pause
