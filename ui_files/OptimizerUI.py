e3225fde20c4b44b5a4929ab27c82fabd4d6caa5
derrian-distro <121591654+derrian-distro@users.noreply.github.com>
2023-06-07 22:32:16 -0400

    changed behavior of queue a bit to better reflect that is actually happening. Added an optimizer args section so that they can be input in the UI.

--- ui_files/OptimizerUI.py
+++ ui_files/OptimizerUI.py
@@ -16,7 +16,8 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QCheckBox, QFormLayout, QLabel,
-    QSizePolicy, QWidget)
+    QPushButton, QScrollArea, QSizePolicy, QTabWidget,
+    QVBoxLayout, QWidget)
 
 from modules.LineEditHighlight import LineEditWithHighlight
 from modules.ScrollOnSelect import (ComboBox, DoubleSpinBox, SpinBox)
@@ -25,10 +26,17 @@ class Ui_optimizer_ui(object):
     def setupUi(self, optimizer_ui):
         if not optimizer_ui.objectName():
             optimizer_ui.setObjectName(u"optimizer_ui")
-        optimizer_ui.resize(400, 317)
-        self.formLayout = QFormLayout(optimizer_ui)
-        self.formLayout.setObjectName(u"formLayout")
-        self.optimizer_type_selector = ComboBox(optimizer_ui)
+        optimizer_ui.resize(400, 349)
+        self.verticalLayout = QVBoxLayout(optimizer_ui)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.tabWidget = QTabWidget(optimizer_ui)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.optimizer_tab_main = QWidget()
+        self.optimizer_tab_main.setObjectName(u"optimizer_tab_main")
+        self.formLayout_2 = QFormLayout(self.optimizer_tab_main)
+        self.formLayout_2.setObjectName(u"formLayout_2")
+        self.optimizer_type_selector = ComboBox(self.optimizer_tab_main)
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
         self.optimizer_type_selector.addItem("")
@@ -42,9 +50,9 @@ class Ui_optimizer_ui(object):
         self.optimizer_type_selector.setObjectName(u"optimizer_type_selector")
         self.optimizer_type_selector.setFocusPolicy(Qt.StrongFocus)
 
-        self.formLayout.setWidget(0, QFormLayout.SpanningRole, self.optimizer_type_selector)
+        self.formLayout_2.setWidget(0, QFormLayout.SpanningRole, self.optimizer_type_selector)
 
-        self.lr_scheduler_selector = ComboBox(optimizer_ui)
+        self.lr_scheduler_selector = ComboBox(self.optimizer_tab_main)
         self.lr_scheduler_selector.addItem("")
         self.lr_scheduler_selector.addItem("")
         self.lr_scheduler_selector.addItem("")
@@ -55,109 +63,138 @@ class Ui_optimizer_ui(object):
         self.lr_scheduler_selector.setObjectName(u"lr_scheduler_selector")
         self.lr_scheduler_selector.setFocusPolicy(Qt.StrongFocus)
 
-        self.formLayout.setWidget(1, QFormLayout.SpanningRole, self.lr_scheduler_selector)
+        self.formLayout_2.setWidget(1, QFormLayout.SpanningRole, self.lr_scheduler_selector)
 
-        self.label = QLabel(optimizer_ui)
+        self.label = QLabel(self.optimizer_tab_main)
         self.label.setObjectName(u"label")
 
-        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label)
+        self.formLayout_2.setWidget(2, QFormLayout.LabelRole, self.label)
 
-        self.main_lr_input = LineEditWithHighlight(optimizer_ui)
+        self.main_lr_input = LineEditWithHighlight(self.optimizer_tab_main)
         self.main_lr_input.setObjectName(u"main_lr_input")
 
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.main_lr_input)
+        self.formLayout_2.setWidget(2, QFormLayout.FieldRole, self.main_lr_input)
 
-        self.unet_lr_enable = QCheckBox(optimizer_ui)
+        self.unet_lr_enable = QCheckBox(self.optimizer_tab_main)
         self.unet_lr_enable.setObjectName(u"unet_lr_enable")
 
-        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.unet_lr_enable)
+        self.formLayout_2.setWidget(3, QFormLayout.LabelRole, self.unet_lr_enable)
 
-        self.unet_lr_input = LineEditWithHighlight(optimizer_ui)
+        self.unet_lr_input = LineEditWithHighlight(self.optimizer_tab_main)
         self.unet_lr_input.setObjectName(u"unet_lr_input")
         self.unet_lr_input.setEnabled(False)
 
-        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.unet_lr_input)
+        self.formLayout_2.setWidget(3, QFormLayout.FieldRole, self.unet_lr_input)
 
-        self.te_lr_enable = QCheckBox(optimizer_ui)
+        self.te_lr_enable = QCheckBox(self.optimizer_tab_main)
         self.te_lr_enable.setObjectName(u"te_lr_enable")
 
-        self.formLayout.setWidget(4, QFormLayout.LabelRole, self.te_lr_enable)
+        self.formLayout_2.setWidget(4, QFormLayout.LabelRole, self.te_lr_enable)
 
-        self.te_lr_input = LineEditWithHighlight(optimizer_ui)
+        self.te_lr_input = LineEditWithHighlight(self.optimizer_tab_main)
         self.te_lr_input.setObjectName(u"te_lr_input")
         self.te_lr_input.setEnabled(False)
 
-        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.te_lr_input)
+        self.formLayout_2.setWidget(4, QFormLayout.FieldRole, self.te_lr_input)
 
-        self.warmup_enable = QCheckBox(optimizer_ui)
+        self.warmup_enable = QCheckBox(self.optimizer_tab_main)
         self.warmup_enable.setObjectName(u"warmup_enable")
 
-        self.formLayout.setWidget(5, QFormLayout.LabelRole, self.warmup_enable)
+        self.formLayout_2.setWidget(5, QFormLayout.LabelRole, self.warmup_enable)
 
-        self.warmup_ratio_input = DoubleSpinBox(optimizer_ui)
+        self.warmup_ratio_input = DoubleSpinBox(self.optimizer_tab_main)
         self.warmup_ratio_input.setObjectName(u"warmup_ratio_input")
         self.warmup_ratio_input.setEnabled(False)
         self.warmup_ratio_input.setFocusPolicy(Qt.StrongFocus)
         self.warmup_ratio_input.setMaximum(1.000000000000000)
         self.warmup_ratio_input.setSingleStep(0.010000000000000)
 
-        self.formLayout.setWidget(5, QFormLayout.FieldRole, self.warmup_ratio_input)
+        self.formLayout_2.setWidget(5, QFormLayout.FieldRole, self.warmup_ratio_input)
 
-        self.label_4 = QLabel(optimizer_ui)
+        self.label_4 = QLabel(self.optimizer_tab_main)
         self.label_4.setObjectName(u"label_4")
 
-        self.formLayout.setWidget(6, QFormLayout.LabelRole, self.label_4)
+        self.formLayout_2.setWidget(6, QFormLayout.LabelRole, self.label_4)
 
-        self.cosine_restart_input = SpinBox(optimizer_ui)
+        self.cosine_restart_input = SpinBox(self.optimizer_tab_main)
         self.cosine_restart_input.setObjectName(u"cosine_restart_input")
         self.cosine_restart_input.setEnabled(False)
         self.cosine_restart_input.setFocusPolicy(Qt.StrongFocus)
         self.cosine_restart_input.setMinimum(1)
 
-        self.formLayout.setWidget(6, QFormLayout.FieldRole, self.cosine_restart_input)
+        self.formLayout_2.setWidget(6, QFormLayout.FieldRole, self.cosine_restart_input)
 
-        self.label_5 = QLabel(optimizer_ui)
+        self.label_5 = QLabel(self.optimizer_tab_main)
         self.label_5.setObjectName(u"label_5")
 
-        self.formLayout.setWidget(7, QFormLayout.LabelRole, self.label_5)
+        self.formLayout_2.setWidget(7, QFormLayout.LabelRole, self.label_5)
 
-        self.poly_power_input = DoubleSpinBox(optimizer_ui)
+        self.poly_power_input = DoubleSpinBox(self.optimizer_tab_main)
         self.poly_power_input.setObjectName(u"poly_power_input")
         self.poly_power_input.setEnabled(False)
         self.poly_power_input.setFocusPolicy(Qt.StrongFocus)
         self.poly_power_input.setSingleStep(0.010000000000000)
         self.poly_power_input.setValue(1.000000000000000)
 
-        self.formLayout.setWidget(7, QFormLayout.FieldRole, self.poly_power_input)
+        self.formLayout_2.setWidget(7, QFormLayout.FieldRole, self.poly_power_input)
 
-        self.min_snr_enable = QCheckBox(optimizer_ui)
+        self.min_snr_enable = QCheckBox(self.optimizer_tab_main)
         self.min_snr_enable.setObjectName(u"min_snr_enable")
 
-        self.formLayout.setWidget(8, QFormLayout.LabelRole, self.min_snr_enable)
+        self.formLayout_2.setWidget(8, QFormLayout.LabelRole, self.min_snr_enable)
 
-        self.min_snr_input = SpinBox(optimizer_ui)
+        self.min_snr_input = SpinBox(self.optimizer_tab_main)
         self.min_snr_input.setObjectName(u"min_snr_input")
         self.min_snr_input.setEnabled(False)
         self.min_snr_input.setFocusPolicy(Qt.StrongFocus)
         self.min_snr_input.setValue(5)
 
-        self.formLayout.setWidget(8, QFormLayout.FieldRole, self.min_snr_input)
+        self.formLayout_2.setWidget(8, QFormLayout.FieldRole, self.min_snr_input)
 
-        self.scale_weight_enable = QCheckBox(optimizer_ui)
+        self.scale_weight_enable = QCheckBox(self.optimizer_tab_main)
         self.scale_weight_enable.setObjectName(u"scale_weight_enable")
 
-        self.formLayout.setWidget(9, QFormLayout.LabelRole, self.scale_weight_enable)
+        self.formLayout_2.setWidget(9, QFormLayout.LabelRole, self.scale_weight_enable)
 
-        self.scale_weight_input = DoubleSpinBox(optimizer_ui)
+        self.scale_weight_input = DoubleSpinBox(self.optimizer_tab_main)
         self.scale_weight_input.setObjectName(u"scale_weight_input")
         self.scale_weight_input.setEnabled(False)
         self.scale_weight_input.setFocusPolicy(Qt.StrongFocus)
         self.scale_weight_input.setValue(1.000000000000000)
 
-        self.formLayout.setWidget(9, QFormLayout.FieldRole, self.scale_weight_input)
+        self.formLayout_2.setWidget(9, QFormLayout.FieldRole, self.scale_weight_input)
+
+        self.tabWidget.addTab(self.optimizer_tab_main, "")
+        self.optimizer_tab_args = QWidget()
+        self.optimizer_tab_args.setObjectName(u"optimizer_tab_args")
+        self.verticalLayout_2 = QVBoxLayout(self.optimizer_tab_args)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.add_opt_button = QPushButton(self.optimizer_tab_args)
+        self.add_opt_button.setObjectName(u"add_opt_button")
+
+        self.verticalLayout_2.addWidget(self.add_opt_button)
+
+        self.scrollArea = QScrollArea(self.optimizer_tab_args)
+        self.scrollArea.setObjectName(u"scrollArea")
+        self.scrollArea.setWidgetResizable(True)
+        self.optimizer_item_widget = QWidget()
+        self.optimizer_item_widget.setObjectName(u"optimizer_item_widget")
+        self.optimizer_item_widget.setGeometry(QRect(0, 0, 376, 269))
+        self.verticalLayout_3 = QVBoxLayout(self.optimizer_item_widget)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.scrollArea.setWidget(self.optimizer_item_widget)
+
+        self.verticalLayout_2.addWidget(self.scrollArea)
+
+        self.tabWidget.addTab(self.optimizer_tab_args, "")
+
+        self.verticalLayout.addWidget(self.tabWidget)
 
 
         self.retranslateUi(optimizer_ui)
+
+        self.tabWidget.setCurrentIndex(1)
+
 
         QMetaObject.connectSlotsByName(optimizer_ui)
     # setupUi
@@ -227,5 +264,8 @@ class Ui_optimizer_ui(object):
 #if QT_CONFIG(tooltip)
         self.scale_weight_input.setToolTip(QCoreApplication.translate("optimizer_ui", u"<html><head/><body><p>An implementation of the idea of max norm regularization. Basically, this will help stabilize network training by limiting the normal of network weights. Might work well for limiting overfitting or baking.</p></body></html>", None))
 #endif // QT_CONFIG(tooltip)
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.optimizer_tab_main), QCoreApplication.translate("optimizer_ui", u"Tab 1", None))
+        self.add_opt_button.setText(QCoreApplication.translate("optimizer_ui", u"Add Optimizer Arg", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.optimizer_tab_args), QCoreApplication.translate("optimizer_ui", u"Tab 2", None))
     # retranslateUi
 
